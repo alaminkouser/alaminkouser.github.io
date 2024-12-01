f4d0ef17d1a8425b9d8cadfeee73cfcd3a414610
AL AMIN KOUSER <alaminkouser@icloud.com>
2024-12-01 12:58:43 +0000

    UPDATE

--- library/convertMarkdownToHtml.ts
+++ library/convertMarkdownToHtml.ts
@@ -1,7 +1,7 @@
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 
 export function convertMarkdownToHtml(markdown: string): string {
-    return `<!DOCTYPE html>
+  return `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
